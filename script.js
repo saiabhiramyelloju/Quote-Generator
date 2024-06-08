fcb8af7e21de763b634c3dd2253e84c9b4605444
Abhiram <110509542+saiabhiramyelloju@users.noreply.github.com>
2024-06-08 22:37:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,20 @@ const quote = [
     "An investment in knowledge pays the best interest.",
     "Education is the passport to the future, for tomorrow belongs to those who prepare for it today.",
     "Learning is a treasure that will follow its owner everywhere.",
+    "Success is not final; failure is not fatal: It is the courage to continue that counts.",
+    "The first step toward success is taken when you refuse to be a captive of the environment in which you first find yourself.",
+    "It's not whether you get knocked down, it's whether you get up.",
+    "Our greatest glory is not in never falling, but in rising every time we fall.",
+    "Strive not to be a success, but rather to be of value.",
+    "Success consists of going from failure to failure without loss of enthusiasm.",
+    "Chase the vision, not the money; the money will end up following you.",
+    "Always be yourself, express yourself, have faith in yourself, do not go out and look for a successful personality and duplicate it.",
+    "Coming together is a beginning; keeping together is progress; working together is success.",
+    "If everyone is moving forward together, then success takes care of itself.",
+    "The price of success is hard work, dedication to the job at hand, and the determination that whether we win or lose, we have applied the best of ourselves to the task at hand.",
+    "A thinker sees his own actions as experiments and questions--as attempts to find out something. Success and failure are for him answers above all.",
+    "There is a price to pay for success, there is also a price to pay for failure. The question is, what price are you willing to pay?",
+    "Doubt kills more dreams than failure ever will.",
 ];
 
 document.getElementById('new-quote').addEventListener('click', () => {
